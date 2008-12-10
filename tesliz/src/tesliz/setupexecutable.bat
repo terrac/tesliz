3d1a84917d7962d46215180f125ba170140ace5a
terra.caines <terra.caines@87ea6ff6-ad55-0410-bec8-edf97f44e4ab>
2008-12-10 19:00:00 +0000

    setupexecutable

---
+++ tesliz/src/tesliz/setupexecutable.bat
@@ -0,0 +1,13 @@
+echo "you need 7zip and upx" for the runthis.bat"
+
+set pythonogre="c:\pythonogre"
+set exeplace="runthis_exe"
+move %pythonogre%\packages_2.5\ogre\renderer\OGRE\* .
+move %pythonogre%\packages_2.5\gui\cegui\OGRE\* .
+
+call runthis
+move %pythonogre%\plugins %exeplace%\plugins
+move %pythonogre%\media %exeplace%\plugins
+move %pythonogre%\demos\media\packs %exeplace%\resources
+move %pythonogre%\packages_2.5\ogre\renderer\OGRE\* %exeplace%
+move %pythonogre%\packages_2.5\gui\cegui\OGRE\* %exeplace%
